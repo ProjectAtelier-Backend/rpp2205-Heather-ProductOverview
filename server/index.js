1478a0a0366a395882a210d05108622abb36e119
hray <101483930+bubsinthemountains@users.noreply.github.com>
2022-11-20 16:15:21 -0500

    successfully getting proper responses for products, product info, and related

--- server/index.js
+++ server/index.js
@@ -2,32 +2,12 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const db = require('../db/index.js');
-const { getProducts } = require('./controller.js');
-
+const router = require('./routes.js');
 
 app.use(express.json());
+app.use('/products', router);
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
 });
 
-
-app.get('/products', (req, res) => {
-  const page = req.query.page || 1;
-  const count = req.query.count || 5;
-  getProducts(page, count)
-    .then((results) => {
-      res.send(results);
-    })
-    .catch(err => console.log(err))
-})
-
-app.get('/products/:product_id', (req, res) => {
-
-})
-
-app.get('/products/:product_id/related', (req, res) => {
-})
-
-app.get('/products/:product_id/styles', (req, res) => {
-})
--- server/models.js
+++ server/models.js
@@ -1,10 +1,9 @@
 const pool = require('../db/index.js');
 
 module.exports = {
-  productList: function (page = 1, count = 5) {
+  readProducts: function (page = 1, count = 5) {
     const max = page * count;
     const min = (page-1)* count;
-
     const query = `SELECT * FROM products WHERE id <= ${max} AND id > ${min}`
     return pool.connect()
       .then((client) => {
@@ -13,12 +12,66 @@ module.exports = {
           .then((res) => {
             client.release()
             return res.rows;
-
           })
           .catch((err) => {
             client.release()
-            console.log('ERROR IN productList', err);
+            console.log(err);
           })
       })
+  },
+  readInfo: function (id) {
+    const query = `
+    SELECT row_to_json(t)
+    FROM (
+      SELECT p.*,
+      (
+        SELECT array_to_json(array_agg(row_to_json(d)))
+        FROM (
+          SELECT f.feature, f.value
+          FROM features f
+          WHERE p.id = product_id
+        ) d
+      ) as features
+    FROM products p
+    WHERE p.id = ${id}
+    ) t`;
+    return pool.connect()
+      .then((client) => {
+        return client
+          .query(query)
+          .then((res) => {
+            client.release()
+            return res.rows[0].row_to_json;
+          })
+          .catch((err) => {
+            client.release()
+            console.log(err);
+          })
+      })
+  },
+
+  readRelated: function(id) {
+    const query = `
+    SELECT array_agg(r.id)
+    FROM related r
+    WHERE r.product_id = ${id}
+    `
+    return pool.connect()
+      .then((client) => {
+        return client
+          .query(query)
+          .then((res) => {
+            client.release()
+            return res.rows[0].array_agg
+          })
+          .catch((err) => {
+            client.release()
+            console.log(err);
+          })
+      })
+  },
+
+  readStyles: function(id) {
+
   }
 }
